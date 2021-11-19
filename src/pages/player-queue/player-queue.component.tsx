4c090c656e4b4b06375ef8d115e752657120b518
bbbneo333 <bbbneo333@gmail.com>
2021-11-20 02:00:10 +0530

    feat: added support for "Now playing" and "Up next" sections in playback queue

--- src/pages/player-queue/player-queue.component.tsx
+++ src/pages/player-queue/player-queue.component.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import classNames from 'classnames/bind';
 import {useSelector} from 'react-redux';
+import * as _ from 'lodash';
 
 import {MediaTrackComponent} from '../../components';
 import {MediaEnums} from '../../enums';
+import {IMediaQueueTrack} from '../../interfaces';
 import {RootState} from '../../reducers';
 import {I18nService, MediaPlayerService} from '../../services';
 
@@ -18,37 +20,69 @@ export function PlayerQueueComponent() {
     mediaPlaybackCurrentMediaTrack,
   } = useSelector((state: RootState) => state.mediaPlayer);
 
+  let mediaPlaybackUpcomingTracks: IMediaQueueTrack[] = [];
+  if (mediaPlaybackCurrentMediaTrack) {
+    const mediaPlaybackCurrentMediaTrackPointer = _.findIndex(mediaTracks, mediaTrack => mediaTrack.queue_entry_id === mediaPlaybackCurrentMediaTrack.queue_entry_id);
+    mediaPlaybackUpcomingTracks = _.slice(mediaTracks, mediaPlaybackCurrentMediaTrackPointer + 1);
+  }
+
   return (
     <div>
       <div className={cx('player-queue')}>
         <div className="container-fluid">
-          <div className={cx('player-queue-header')}>
-            <div className="row">
-              <div className="col-12">
-                <div className={cx('player-queue-header-label')}>
-                  {I18nService.getString('label_player_queue_header')}
+          {!mediaPlaybackCurrentMediaTrack && _.isEmpty(mediaTracks) && (
+            <div className={cx('player-queue-section')}>
+              <div className="row">
+                <div className="col-12">
+                  <div className={cx('player-queue-section-header', 'player-queue-empty')}>
+                    {I18nService.getString('label_player_queue_empty')}
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-          <div className={cx('player-queue-tracklist')}>
-            <div className="row">
-              {mediaTracks.map(mediaTrack => (
-                <MediaTrackComponent
-                  key={mediaTrack.id}
-                  mediaTrack={mediaTrack}
-                  handleOnPlayButtonClick={() => {
-                    MediaPlayerService.playMediaTrackFromQueue(mediaTrack);
-                  }}
-                  isPlaying={
-                    mediaPlaybackState === MediaEnums.MediaPlaybackState.Playing
-                    && mediaPlaybackCurrentMediaTrack
-                    && mediaPlaybackCurrentMediaTrack.queue_entry_id === mediaTrack.queue_entry_id
-                  }
-                />
-              ))}
+          )}
+          {mediaPlaybackCurrentMediaTrack && (
+            <div className={cx('player-queue-section')}>
+              <div className="row">
+                <div className="col-12">
+                  <div className={cx('player-queue-section-header')}>
+                    {I18nService.getString('label_player_queue_current_track')}
+                  </div>
+                  <div className={cx('player-queue-section-content')}>
+                    <MediaTrackComponent
+                      mediaTrack={mediaPlaybackCurrentMediaTrack}
+                      isPlaying={mediaPlaybackState === MediaEnums.MediaPlaybackState.Playing}
+                      handleOnPlayButtonClick={() => {
+                        MediaPlayerService.playMediaTrackFromQueue(mediaPlaybackCurrentMediaTrack);
+                      }}
+                    />
+                  </div>
+                </div>
+              </div>
             </div>
-          </div>
+          )}
+          {!_.isEmpty(mediaPlaybackUpcomingTracks) && (
+            <div className={cx('player-queue-section')}>
+              <div className="row">
+                <div className="col-12">
+                  <div className={cx('player-queue-section-header')}>
+                    {I18nService.getString('label_player_queue_upcoming_tracks')}
+                  </div>
+                  <div className={cx('player-queue-section-content')}>
+                    {mediaPlaybackUpcomingTracks.map(mediaTrack => (
+                      <MediaTrackComponent
+                        key={mediaTrack.id}
+                        mediaTrack={mediaTrack}
+                        handleOnPlayButtonClick={() => {
+                          MediaPlayerService.playMediaTrackFromQueue(mediaTrack);
+                        }}
+                      />
+                    ))}
+                  </div>
+                </div>
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </div>
