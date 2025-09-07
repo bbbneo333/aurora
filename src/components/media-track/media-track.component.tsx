27aa69b370f5deff642cfa36671b5f9ff9641da7
bbbneo333 <bbbneo333@gmail.com>
2025-09-07 19:34:08 +0530

    fixed aria props for media track

--- src/components/media-track/media-track.component.tsx
+++ src/components/media-track/media-track.component.tsx
@@ -49,8 +49,9 @@ export const MediaTrack = React.forwardRef<HTMLDivElement, MediaTrackProps<IMedi
   });
 
   return (
-    // eslint-disable-next-line jsx-a11y/no-static-element-interactions
     <div
+      role="row"
+      tabIndex={0}
       {...rest}
       ref={ref}
       className={cx('media-track', className)}
@@ -63,33 +64,35 @@ export const MediaTrack = React.forwardRef<HTMLDivElement, MediaTrackProps<IMedi
         if (Events.isEnterKey(e) || Events.isSpaceKey(e)) toggle();
       }}
     >
-      <div className={cx('media-track-section')}>
-        <MediaPlaybackButton
-          isPlaying={isTrackPlaying}
-          className={cx('media-track-playback-button')}
-          onPlay={play}
-          onPause={pause}
-        />
-      </div>
-      {!disableCover && (
+      <div className={cx('media-track-content')}>
         <div className={cx('media-track-section')}>
-          <MediaCoverPicture
-            mediaPicture={mediaTrack.track_cover_picture}
-            mediaPictureAltText={mediaTrack.track_name}
-            className={cx('media-track-cover')}
+          <MediaPlaybackButton
+            isPlaying={isTrackPlaying}
+            className={cx('media-track-playback-button')}
+            onPlay={play}
+            onPause={pause}
           />
         </div>
-      )}
-      <div className={cx('media-track-section')}>
-        <MediaTrackInfoComponent
-          mediaTrack={mediaTrack}
-          disableAlbumLink={disableAlbumLink}
-          className={cx('media-track-info')}
-        />
-      </div>
-      <div className={cx('media-track-section', 'media-track-side-column')}>
-        <div className={cx('media-track-duration')}>
-          {DateTimeUtils.formatSecondsToDuration(mediaTrack.track_duration)}
+        {!disableCover && (
+          <div className={cx('media-track-section')}>
+            <MediaCoverPicture
+              mediaPicture={mediaTrack.track_cover_picture}
+              mediaPictureAltText={mediaTrack.track_name}
+              className={cx('media-track-cover')}
+            />
+          </div>
+        )}
+        <div className={cx('media-track-section')}>
+          <MediaTrackInfoComponent
+            mediaTrack={mediaTrack}
+            disableAlbumLink={disableAlbumLink}
+            className={cx('media-track-info')}
+          />
+        </div>
+        <div className={cx('media-track-section', 'media-track-side-column')}>
+          <div className={cx('media-track-duration')}>
+            {DateTimeUtils.formatSecondsToDuration(mediaTrack.track_duration)}
+          </div>
         </div>
       </div>
     </div>
