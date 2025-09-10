97d41419a3388ab61b05a1c7df751996fe24760c
bbbneo333 <bbbneo333@gmail.com>
2025-09-10 15:07:57 +0530

    upgraded node to v20.19.5

--- babel.config.js
+++ babel.config.js
@@ -41,7 +41,7 @@ module.exports = (api) => {
       [require('@babel/plugin-proposal-decorators'), { legacy: true }],
       require('@babel/plugin-proposal-function-sent'),
       require('@babel/plugin-proposal-export-namespace-from'),
-      require('@babel/plugin-proposal-numeric-separator'),
+      // require('@babel/plugin-proposal-numeric-separator'),
       require('@babel/plugin-proposal-throw-expressions'),
 
       // stage - 3
