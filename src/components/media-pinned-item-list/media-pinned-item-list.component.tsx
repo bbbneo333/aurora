95a2a5d7467474f5520d24121f3e13a017c67b85
bbbneo333 <bbbneo333@gmail.com>
2025-09-18 19:26:45 +0530

    added support for deleting playlist upon pressing delete on pinned item list

--- src/components/media-pinned-item-list/media-pinned-item-list.component.tsx
+++ src/components/media-pinned-item-list/media-pinned-item-list.component.tsx
@@ -1,14 +1,18 @@
 import React, { useCallback, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import classNames from 'classnames/bind';
+import { isNil } from 'lodash';
 
+import { useModal } from '../../contexts';
+import { MediaCollectionItemType } from '../../enums';
+import { IMediaPinnedItem } from '../../interfaces';
 import { MediaCollectionService, MediaPinnedItemService } from '../../services';
 import { selectSortedPinnedItems } from '../../selectors';
-import { IMediaPinnedItem } from '../../interfaces';
 
 import { InteractiveList } from '../interactive-list/interactive-list.component';
 import { MediaCollectionItem } from '../media-collection-item/media-collection-item.component';
 import { MediaCollectionContextMenu, MediaCollectionContextMenuItem } from '../media-collection-context-menu/media-collection-context-menu.component';
+import { MediaPlaylistDeleteModal } from '../media-playlist-delete-modal/media-playlist-delete-modal.component';
 
 import styles from './media-pinned-item-list.component.css';
 
@@ -17,6 +21,7 @@ const cx = classNames.bind(styles);
 export function MediaPinnedItemList() {
   const sortedMediaPinnedItems = useSelector(selectSortedPinnedItems);
   const contextMenuId = 'media-pinned-item-list-context-menu';
+  const { showModal } = useModal();
 
   useEffect(() => {
     MediaPinnedItemService.loadPinnedItems();
@@ -27,6 +32,27 @@ export function MediaPinnedItemList() {
     MediaPinnedItemService.loadPinnedItems();
   }, []);
 
+  const handleItemsDelete = useCallback((ids: string[]) => new Promise<boolean>((resolve) => {
+    const pinnedItemId = ids[0];
+    const pinnedItem = sortedMediaPinnedItems.find(item => item.pinned_item_id === pinnedItemId);
+
+    if (pinnedItem && pinnedItem.type === MediaCollectionItemType.Playlist) {
+      showModal(MediaPlaylistDeleteModal, {
+        mediaPlaylistId: pinnedItem.id,
+      }, {
+        onComplete: (result) => {
+          resolve(!isNil(result?.deletedId));
+          MediaPinnedItemService.loadPinnedItems(); // TODO: Remove this, items should be automatically removed on collection item removal
+        },
+      });
+    } else {
+      resolve(false);
+    }
+  }), [
+    showModal,
+    sortedMediaPinnedItems,
+  ]);
+
   return (
     <>
       <InteractiveList
@@ -34,7 +60,9 @@ export function MediaPinnedItemList() {
         sortable
         className={cx('media-pinned-item-list')}
         items={sortedMediaPinnedItems}
+        getItemId={item => item.pinned_item_id}
         onItemsSorted={handleItemsSorted}
+        onItemsDelete={handleItemsDelete}
       >
         {pinnedItem => (
           <MediaCollectionItem
