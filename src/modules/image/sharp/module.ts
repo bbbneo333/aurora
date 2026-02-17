3b34d3308fb75d66c5c18cc2eee041458baed790
bbbneo333 <bbbneo333@gmail.com>
2026-02-17 17:00:34 +0530

    chore: init SharpModule

---
+++ src/modules/image/sharp/module.ts
@@ -0,0 +1,45 @@
+import path from 'path';
+import sharp from 'sharp';
+
+import { ImageFileExtensions } from '../../../enums';
+import { IAppMain } from '../../../interfaces';
+
+import { CryptoService } from '../../crypto';
+import { FileSystemUtils } from '../../file-system';
+
+import { SharpImageScaleOptions } from './types';
+
+export class SharpModule {
+  private readonly app: IAppMain;
+  private readonly imageExtension = ImageFileExtensions.JPG;
+  private readonly imagesDataDir = 'Images';
+
+  constructor(app: IAppMain) {
+    this.app = app;
+  }
+
+  async scaleImage(data: Buffer | string, options: SharpImageScaleOptions): Promise<string> {
+    const source = typeof data === 'string' ? data : Buffer.from(data);
+    const { width, height } = options;
+
+    const imageCacheDir = this.app.createDataDir(this.imagesDataDir);
+    // we use image (path or buffer data) and dimensions as cache key
+    const imageCacheKey = CryptoService.sha1(data, `${width}x${height}`);
+    const imageCachePath = path.join(imageCacheDir, `${imageCacheKey}.${this.imageExtension}`);
+
+    // if file already exists, return that
+    // otherwise create and store new image
+    if (FileSystemUtils.isFile(imageCachePath)) {
+      return imageCachePath;
+    }
+
+    await sharp(source)
+      .resize(width, height, {
+        fit: 'cover',
+        position: 'center',
+      })
+      .toFile(imageCachePath);
+
+    return imageCachePath;
+  }
+}
